faf0622d58ef7de9ef7088d1e71c7583edcf64f4
shan14692 <mosharraf15-14692@diu.edu.bd>
2024-05-02 15:51:30 +0600

    shan_92

---
+++ lib/utils/work_utils.dart
@@ -0,0 +1,19 @@
+import 'package:folio/utils/utils.dart';
+
+class WorkUtils {
+  static const List<String> logos = [
+    StaticUtils.university,
+    StaticUtils.fullterIsb,
+    StaticUtils.dsc,
+    StaticUtils.sastaticket,
+  ];
+
+  static const List<String> communityLinks = [
+    "https://github.com/shan14692",
+    "https://github.com/shan14692",
+    "https://github.com/shan14692",
+    "https://github.com/shan14692"
+  ];
+
+  static const List<double> communityLogoHeight = [60.0, 70.0, 30.0, 70.0];
+}
